e7311e71507e945a11942c89da755e8b3881a16a
pranavnayak6 <pranavnayak2628@gmail.com>
2026-06-05 12:24:16 +0530

    sadasd

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/abc")
+def home():
+    return "Hello world"
+
+if __name__ =="__main__":
+    app.run(debug=True)
